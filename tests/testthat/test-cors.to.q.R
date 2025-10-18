8962004172d7ee9b24740e218b56d221358d8d83
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2025-10-18 23:29:33 +1100

    Added further unit tests, incl. reorganizing unit tests for .to.-functions

--- tests/testthat/test-cors.to.q.R
+++ tests/testthat/test-cors.to.q.R
@@ -1,3 +1,14 @@
-test_that("cors.to.q works", {
-  expect_equal(2 * 2, 4)
+test_that("cors.to.q / q.to.cors works", {
+    expect_equal(q.to.cors(q = 0.10,         rho1 = 0.5, verbose = FALSE),
+                 list(q =  0.1, delta = -0.0712027, rho1 = 0.5, rho2 = 0.5712027), tolerance = 1e-6)
+    expect_equal(q.to.cors(q = 0.30,         rho1 = 0.5, verbose = FALSE),
+                 list(q =  0.3, delta = -0.1907068, rho1 = 0.5, rho2 = 0.6907068), tolerance = 1e-6)
+    expect_equal(q.to.cors(q = 0.50,         rho1 = 0.5, verbose = FALSE),
+                 list(q =  0.5, delta = -0.2815365, rho1 = 0.5, rho2 = 0.7815365), tolerance = 1e-6)
+    expect_equal(cors.to.q(rho2 = 0.5712027, rho1 = 0.5, verbose = FALSE),
+                 list(q = -0.1, delta = -0.0712027, rho1 = 0.5, rho2 = 0.5712027), tolerance = 1e-6)
+    expect_equal(cors.to.q(rho2 = 0.6907068, rho1 = 0.5, verbose = FALSE),
+                 list(q = -0.3, delta = -0.1907068, rho1 = 0.5, rho2 = 0.6907068), tolerance = 1e-6)
+    expect_equal(cors.to.q(rho2 = 0.7815365, rho1 = 0.5, verbose = FALSE),
+                 list(q = -0.5, delta = -0.2815365, rho1 = 0.5, rho2 = 0.7815365), tolerance = 1e-6)
 })
--- tests/testthat/test-factorial.contrasts.R
+++ tests/testthat/test-factorial.contrasts.R
@@ -1,3 +1,20 @@
 test_that("factorial.contrasts works", {
-  expect_equal(2 * 2, 4)
+    expect_equal(2 * 2, 4)
+#    expect_equal(factorial.contrasts(factor.levels = 3, coding = "treatment", verbose = FALSE)$model.matrix,
+#                 matrix(c(1, 0, 0, 1, -1, -1), nrow = 2, dimnames = list(c("A1", "A2"), c("A1", "A2", "A3"))))
+#    expect_equal(factorial.contrasts(factor.levels = 3, coding = "treatment", verbose = FALSE)$contrast.matrix,
+#                 matrix(c(1, 0, 0, 1, -1, -1), nrow = 2, dimnames = list(c("A1", "A2"), c("A1", "A2", "A3"))))
+
+#    expect_equal(suppressMessages(factorial.contrasts(factor.levels = c(2, 3), coding = "sum", verbose = FALSE))$contrast.matrix
+
+#    expect_equal(suppressMessages(factorial.contrasts(factor.levels = c(2, 3), coding = "sum", verbose = FALSE))$contrast.matrix,
+#                 matrix(c(+1/6, +1/3, -1/6, +1/3, -1/6, +1/6, -1/6, +1/3, -1/6, +1/3, +1/6, -1/6, -1/6, -1/6, -1/6,
+#                          -1/6, +1/3, -1/6, -1/3, +1/6, -1/6, -1/6, +1/3, +1/6, -1/3, -1/6, -1/6, -1/6, +1/6, +1/6),
+#                        nrow = 5, dimnames = list(c("A1", "B1", "B2", "A1:B1", "A1:B2"),
+#                                                  c("A1:B1", "A1:B2", "A1:B3", "A2:B1", "A2:B2", "A2:B3"))))
+
+#    factorial.contrasts(factor.levels = 3, coding = "helmert", verbose = FALSE)$model.matrix
+#    factorial.contrasts(factor.levels = 3, coding = "helmert", verbose = FALSE)$contrast.matrix
+#    factorial.contrasts(factor.levels = 3, coding = "poly",    verbose = FALSE)$model.matrix
+#    factorial.contrasts(factor.levels = 3, coding = "poly",    verbose = FALSE)$contrast.matrix
 })
